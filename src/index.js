01b94770fb9a850bb3e7340bdd323eeb20382b7a
Oblivium <simonberardi@gmail.com>
2020-12-11 19:06:01 +0100

    Added homepage first draft

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import WishlistList from './wishlists/WishlistList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WishlistList />
   </React.StrictMode>,
   document.getElementById('root')
 );
